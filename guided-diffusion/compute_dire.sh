9488fbfdd3e4ceaba28eddc2c63a145a7174a7be
Lumostine <lumostine@192.168.2.27>
2023-12-08 21:34:55 +0700

    add CUDA

--- guided-diffusion/compute_dire.sh
+++ guided-diffusion/compute_dire.sh
@@ -6,4 +6,7 @@ MODEL_PATH="/kaggle/input/classifier-ckpt/256x256_diffusion_uncond.pt" # "models
 SAMPLE_FLAGS="--batch_size 16 --num_samples 1000  --timestep_respacing ddim20 --use_ddim True"
 SAVE_FLAGS="--images_dir /kaggle/working/dataset/images/test/real --recons_dir /kaggle/working/dataset/recons/test/real --dire_dir /kaggle/working/dataset/dire/test/real"
 MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond False --diffusion_steps 1000 --dropout 0.1 --image_size 256 --learn_sigma True --noise_schedule linear --num_channels 256 --num_head_channels 64 --num_res_blocks 2 --resblock_updown True --use_fp16 False --use_scale_shift_norm True"
+
+export CUDA_VISIBLE_DEVICES=0
+
 python /kaggle/working/DetectArtForensics/guided-diffusion/compute_dire.py --model_path $MODEL_PATH $MODEL_FLAGS  $SAVE_FLAGS $SAMPLE_FLAGS --has_subfolder True
