1488bb624aaf53b0fbddd8328f404ccd8182001c
Lumostine <lumostine@Lumosmac.local>
2023-12-20 18:03:26 +0700

    add

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 ### make sure that you have modified the EXP_NAME, DATASETS, DATASETS_TEST
 eval "$(conda shell.bash hook)"
 #conda activate dire
-EXP_NAME="gray_50"
+EXP_NAME="gray_adamw"
 DATASETS="dire"
 DATASETS_TEST="dire"
 python train.py --gpus 0 --exp_name $EXP_NAME datasets $DATASETS datasets_test $DATASETS_TEST
